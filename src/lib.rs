227cf98d674bb3b411a2966935fc778b37fa2a0e
Andrey Semenov <i@itmage.ru>
2026-04-04 13:51:07 +0300

    fix: remove unused tests

--- src/lib.rs
+++ src/lib.rs
@@ -9,19 +9,3 @@ pub mod utils;
 pub use bot::Bot;
 pub use dispatcher::{Context, Dispatcher};
 pub use types::{KnownUpdate, Update, UpdateKind, VkError};
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn it_works() {
-        dotenvy::dotenv().ok();
-
-        let token = std::env::var("VKOXIDE_TOKEN").unwrap();
-        let group_id = std::env::var("VKOXIDE_GROUP_ID").unwrap();
-        let bot = Bot::new(token, group_id);
-
-        Dispatcher::builder(bot).build().dispatch().await.unwrap();
-    }
-}
