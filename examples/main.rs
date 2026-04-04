5cf5c67d440d6ec216087eccfdf23d47cebe426f
Andrey Semenov <i@itmage.ru>
2026-04-04 12:50:26 +0300

    feat: added state example

--- examples/main.rs
+++ examples/main.rs
@@ -1,4 +1,9 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
 use vkoxide::{Bot, Context, Dispatcher, KnownUpdate, Update, UpdateKind};
+
+struct MyState {
+    pub message_count: AtomicUsize,
+}
 
 #[tokio::main]
 async fn main() {
@@ -8,7 +13,12 @@ async fn main() {
     let group_id = std::env::var("VKOXIDE_GROUP_ID").unwrap();
     let bot = Bot::new(token, group_id);
 
+    let app_state = MyState {
+        message_count: AtomicUsize::new(0),
+    };
+
     let dispatcher = Dispatcher::builder(bot)
+        .state(app_state)
         .add_handler(
             |update: &Update| -> bool {
                 matches!(
@@ -16,10 +26,20 @@ async fn main() {
                     UpdateKind::Known(KnownUpdate::MessageNew { .. })
                 )
             },
-            |update: Update, ctx: Context<()>| async move {
+            |update: Update, ctx: Context<MyState>| async move {
                 if let UpdateKind::Known(KnownUpdate::MessageNew { object }) = update.kind {
-                    let answer = format!("Вы сказали: {}", object.message.text);
-                    println!("Новое сообщение от {}: отвечаем...", object.message.from_id);
+                    let current_count = ctx.state.message_count.fetch_add(1, Ordering::Relaxed) + 1;
+
+                    let answer = format!(
+                        "Вы сказали: {}\nВы - {}-й написавший мне человек!",
+                        object.message.text, current_count
+                    );
+
+                    println!(
+                        "Новое сообщение от {}, это уже {} сообщение...",
+                        object.message.from_id, current_count
+                    );
+
                     ctx.bot
                         .send_message(object.message.from_id, &answer)
                         .await?;
